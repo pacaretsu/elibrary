113e4de8ff2b587e2b9411a95ed632087e4ecdbc
Baldwin College <baldwin@baldwin-OptiPlex-390.(none)>
2013-09-26 05:22:54 +0000

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Elibrary::Application.config.secret_token = '5506f3c6da5a4faad73549c3357271e914a60e8568ba00c08369a1356b045718f57371730df88c42493f19b18e513993bbf4ede99c43f741df888c538da583e3'
